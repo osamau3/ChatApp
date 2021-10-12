d9cd829c68ff4d2ecad4dfc48709f46789c790db
osama <osama.yousryu3@gmail.com>
2021-10-12 15:28:19 +0200

    Fix: send message when press enter

--- src/components/ChatFooter/ChatFooter.js
+++ src/components/ChatFooter/ChatFooter.js
@@ -21,24 +21,40 @@ export default function ChatFooter(props) {
         }
     }
 
-    const handleSendMessage = () => {
-        // simulate push message to server
-        setTimeout(() => {
-            props.onSendMessge({
-                date: Date.now(),
-                message: message,
-                from: 'Osama Yousry',
-                isIncomming: false,
-            })
-        }, 500)
-        setMessage('')
-        setIsBtnDisabled(true)
+    const handleSendMessage = (event) => {
+        if(event.key === 'Enter' || event.target.ariaLabel === 'sendMessageBtn') {
+            // simulate push message to server
+            setTimeout(() => {
+                props.onSendMessge({
+                    date: Date.now(),
+                    message: message,
+                    from: 'Osama Yousry',
+                    isIncomming: false,
+                })
+            }, 500)
+            setMessage('')
+            setIsBtnDisabled(true)
+        }
     }
 
     return (
         <ChatFooterSC onClick={() => inputRef.current.focus() }>
-            <input ref={inputRef} aria-label="messageInput" placeholder="Type a message..." value={message} onChange={handleMessaging}/>
-            <button aria-label="sendMessageBtn" className={`${isBtnDisabled && 'disabled'}`} disabled={isBtnDisabled} onClick={handleSendMessage}>Send</button>
+            <input
+                ref={inputRef}
+                aria-label="messageInput"
+                placeholder="Type a message..."
+                value={message}
+                onChange={handleMessaging}
+                onKeyPress={message && handleSendMessage}
+            />
+            <button
+                aria-label="sendMessageBtn"
+                className={`${isBtnDisabled && 'disabled'}`}
+                disabled={isBtnDisabled}
+                onClick={handleSendMessage}
+            >
+                Send
+            </button>
         </ChatFooterSC>
     )
 }
